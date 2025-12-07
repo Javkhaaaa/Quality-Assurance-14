2cf04b2be024aa9fca6a1de0ad8e74fc277eb141
Bayarjavhlan Munkhsaihan <jambaaok13@gmail.com>
2025-12-07 21:14:09 +0800

    Calculator классын main метод нэмсэн

--- src/main/java/lab14/sict/must/edu/mn/Calculator.java
+++ src/main/java/lab14/sict/must/edu/mn/Calculator.java
@@ -25,5 +25,8 @@ public class Calculator {
     public double subtract(double a, double b) {
         return a - b;
     }
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+    }
 }
 
