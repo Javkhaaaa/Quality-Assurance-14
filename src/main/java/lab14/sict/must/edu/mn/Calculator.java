3662a55945271869b3ac98b3bec992ec88ae5e74
Bayarjavhlan Munkhsaihan <jambaaok13@gmail.com>
2025-12-07 21:18:20 +0800

    Calculator классын main метод устгаж, JaCoCo тохиргоог засварласан

--- src/main/java/lab14/sict/must/edu/mn/Calculator.java
+++ src/main/java/lab14/sict/must/edu/mn/Calculator.java
@@ -25,8 +25,5 @@ public class Calculator {
     public double subtract(double a, double b) {
         return a - b;
     }
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-    }
 }
 
